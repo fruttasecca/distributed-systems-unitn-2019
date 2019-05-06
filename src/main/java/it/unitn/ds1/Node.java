9079164e449d2ea13a389e1a9221cfe130d60252
fruttasecca <jacopo.gobbi@studenti.unitn.it>
2019-05-06 10:24:23 +0200

    added ActorInfo class which contains both an actor reference and id, to facilitate logging

--- src/main/java/it/unitn/ds1/Node.java
+++ src/main/java/it/unitn/ds1/Node.java
@@ -10,18 +10,19 @@ import akka.actor.Props;
 
 import java.util.concurrent.TimeUnit;
 
+import akka.actor.dsl.Creators;
 import scala.concurrent.duration.FiniteDuration;
 
 class Node extends AbstractActor {
     private final int id;    // ID of the current actor
-    private List<ActorRef> neighbours; // neighbourhood of this node
+    private List<ActorInfo> neighbours; // neighbourhood of this node (list of pairs of (actoref, int)
     private final boolean be_greedy; // should the node be greedy in adding to the queue (add itselfs on top, always)
-    private ActorRef holder; // who is the holder for this node
+    private ActorInfo holder; // who is the holder for this node
     private boolean using; // is the node in the CS?
     private boolean asked; // has the node asked for the PRIVILEGE token to its holder?
     private boolean in_recovery_mode; // is the node in recovery mode?
     private boolean in_failure_mode; // is the node in failure mode?
-    private Deque<ActorRef> request_queue; // Queue where to store requests incoming from neighbours.
+    private Deque<ActorInfo> request_queue; // Queue where to store requests incoming from neighbours.
     private int total_advise_msgs_received; // amount of advise msgs received during this recovery
 
     // durations
@@ -85,25 +86,48 @@ class Node extends AbstractActor {
     Message Classes
     ###############################
     */
+    public static class NodeToNodeMsg implements Serializable {
+        public final int senderId;
+
+        public NodeToNodeMsg(final int senderId) {
+            this.senderId = senderId;
+
+        }
+    }
 
     // Messages sent from node to node
     //###############################
-    public static class InitializeMsg implements Serializable {
+    public static class InitializeMsg extends NodeToNodeMsg {
+        public InitializeMsg(final int senderId) {
+            super(senderId);
+        }
     }
 
-    public static class RequestMsg implements Serializable {
+    public static class RequestMsg extends NodeToNodeMsg {
+        public RequestMsg(final int senderId) {
+            super(senderId);
+        }
     }
 
-    public static class PrivilegeMsg implements Serializable {
+    public static class PrivilegeMsg extends NodeToNodeMsg {
+        public PrivilegeMsg(final int senderId) {
+            super(senderId);
+        }
     }
 
-    public static class PrivilegeAndRequestMsg implements Serializable {
+    public static class PrivilegeAndRequestMsg extends NodeToNodeMsg {
+        public PrivilegeAndRequestMsg(final int senderId) {
+            super(senderId);
+        }
     }
 
-    public static class RestartMsg implements Serializable {
+    public static class RestartMsg extends NodeToNodeMsg {
+        public RestartMsg(final int senderId) {
+            super(senderId);
+        }
     }
 
-    public static class AdviseMsg implements Serializable {
+    public static class AdviseMsg extends NodeToNodeMsg {
         // if the node requesting the Advise message is the holder for the node sending the message.
         public final boolean you_are_my_holder;
         // if the node sending the Advise message has asked for the token to its holder.
@@ -111,7 +135,9 @@ class Node extends AbstractActor {
         // if the node requesting the Advise message is in the request queue of the node sending the message.
         public final boolean you_asked_me;
 
-        public AdviseMsg(boolean you_are_my_holder, boolean asked, boolean you_asked_me) {
+        public AdviseMsg(final int senderId, final boolean you_are_my_holder,
+                         final boolean asked, final boolean you_asked_me) {
+            super(senderId);
             this.you_are_my_holder = you_are_my_holder;
             this.asked = asked;
             this.you_asked_me = you_asked_me;
@@ -174,63 +200,63 @@ class Node extends AbstractActor {
     //###############################
 
     private void onInitializeMsg(InitializeMsg msg) {
-        logReceivingMsg("Initialize", -1);
+        logReceivingMsg("Initialize", msg.senderId);
 
-        holder = getSender();
+        holder = new ActorInfo(getSender(), msg.senderId);
         // flood the rest of the neighbours
-        for (ActorRef neigh : neighbours) {
-            if (neigh != holder) {
-                logSendingMsg("Initialize", -1);
-                neigh.tell(new InitializeMsg(), getSelf());
+        for (ActorInfo neigh : neighbours) {
+            if (neigh.reference != holder.reference) {
+                logSendingMsg("Initialize", neigh.id);
+                neigh.reference.tell(new InitializeMsg(id), getSelf());
             }
         }
     }
 
     private void onRequestMsg(RequestMsg msg) {
-        logReceivingMsg("Request", -1);
-        request_queue.add(getSender());
+        logReceivingMsg("Request", msg.senderId);
+        request_queue.add(new ActorInfo(getSender(), msg.senderId));
         assignPrivilege();
         makeRequest();
     }
 
     private void onPrivilegeMsg(PrivilegeMsg msg) {
-        logReceivingMsg("Privilege", -1);
-        holder = getSelf();
+        logReceivingMsg("Privilege", msg.senderId);
+        holder = new ActorInfo(getSelf(), id);
         assignPrivilege();
         makeRequest();
     }
 
     private void onPrivilegeAndRequestMsg(PrivilegeAndRequestMsg msg) {
-        logReceivingMsg("PrivilegeAndRequest", -1);
-        holder = getSelf();
-        request_queue.add(getSender());
+        logReceivingMsg("PrivilegeAndRequest", msg.senderId);
+        holder = new ActorInfo(getSelf(), id);
+        request_queue.add(new ActorInfo(getSender(), msg.senderId));
         assignPrivilege();
         makeRequest();
     }
 
     private void onRestartMsg(RestartMsg msg) {
-        logReceivingMsg("Restart", -1);
-        boolean you_are_my_holder = getSender() == holder;
-        boolean you_asked_me = request_queue.contains(getSender());
+        logReceivingMsg("Restart", msg.senderId);
+        boolean you_are_my_holder = getSender() == holder.reference;
+        boolean you_asked_me = request_queue.contains(new ActorInfo(getSender(), msg.senderId));
 
-        logSendingMsg("Advise", -1);
-        getSender().tell(new AdviseMsg(you_are_my_holder, asked, you_asked_me), getSelf());
+        logSendingMsg("Advise", msg.senderId);
+        getSender().tell(new AdviseMsg(id, you_are_my_holder, asked, you_asked_me), getSelf());
     }
 
     private void onAdviseMsg(AdviseMsg msg) {
-        logReceivingMsg("Advise", -1);
+        logReceivingMsg("Advise", msg.senderId);
         total_advise_msgs_received++;
 
         // note that msg.asked and mgs.you_asked_me are mutually exclusive
-        if (msg.asked) {
+        if (msg.asked && msg.you_are_my_holder) {
             // if the other node asked us for the PRIVILEGE we enqueue it
-            request_queue.add(getSender());
+            request_queue.add(new ActorInfo(getSender(), msg.senderId));
         } else if (!msg.you_are_my_holder) {
             /*
             if the receiving node is not the holder of the sender, then the sender is the holder
             of the receiver (this node)
              */
-            holder = getSender();
+            holder = new ActorInfo(getSender(), msg.senderId);
 
             // did we ask our holder for the privilege?
             if (msg.you_asked_me)
@@ -250,7 +276,7 @@ class Node extends AbstractActor {
     //###############################
 
     private void onSelfExitCSMsg(SelfExitCSMsg msg) {
-        logReceivingMsg("ExitCS", -1);
+        logReceivingMsg("ExitCS", id);
         String to_log = String.format("Node %02d exited CS", this.id);
         history.append(to_log);
         System.out.print(to_log);
@@ -261,7 +287,7 @@ class Node extends AbstractActor {
     }
 
     private void onSelfStartRecoveryMsg(SelfStartRecoveryMsg msg) {
-        logReceivingMsg("SelfStartRecoveryMsg", -1);
+        logReceivingMsg("SelfStartRecoveryMsg", id);
         // we are now in recovery mode
         in_failure_mode = false;
         in_recovery_mode = true;
@@ -270,15 +296,15 @@ class Node extends AbstractActor {
         // throughout the recovery these fields will be modified/updated incrementally each time
         // we receive ad advise msg
         asked = false;
-        holder = getSelf();
+        holder = new ActorInfo(getSelf(), id);
         request_queue = new ArrayDeque<>();
         // we will use this to decide when the recovery is over
         total_advise_msgs_received = 0;
 
         // ask all neighbours for Advise Msgs
-        for (ActorRef neigh : neighbours) {
-            logSendingMsg("RESTART", -1);
-            neigh.tell(new RestartMsg(), getSelf());
+        for (ActorInfo neigh : neighbours) {
+            logSendingMsg("RESTART", neigh.id);
+            neigh.reference.tell(new RestartMsg(id), getSelf());
         }
     }
 
@@ -286,7 +312,15 @@ class Node extends AbstractActor {
     //###############################
 
     private void onSystemNodeNeighbourhoodMsg(SystemNodeNeighbourhoodMsg msg) {
-        this.neighbours = msg.neighbourhood;
+        // build all pairs of (actor reference, id)
+        List<ActorInfo> neighbours = new ArrayList<>();
+        Iterator<ActorRef> refIte = msg.neighbourhood.iterator();
+        Iterator<Integer> idIte = msg.neighbourhood_ids.iterator();
+        while (refIte.hasNext() && idIte.hasNext()) {
+            neighbours.add(new ActorInfo(refIte.next(), idIte.next()));
+        }
+        this.neighbours = Collections.unmodifiableList(neighbours);
+
 
         String to_log = String.format("Node %02d neighbourhood: %s\n", this.id, msg.neighbourhood_ids);
         history.append(to_log);
@@ -296,17 +330,18 @@ class Node extends AbstractActor {
     private void onSystemInitMsg(SystemInitMsg msg) {
         logReceivingMsg("SystemInitMessage", -1);
         // i am the holder now
-        holder = getSelf();
+        holder = new ActorInfo(getSelf(), id);
         // flood neighbours
-        for (ActorRef neigh : neighbours) {
-            logSendingMsg("Initialize", -1);
-            neigh.tell(new InitializeMsg(), getSelf());
+
+        for (ActorInfo neigh : neighbours) {
+            logSendingMsg("Initialize", neigh.id);
+            neigh.reference.tell(new InitializeMsg(id), getSelf());
         }
     }
 
     private void onSystemWantCSMsg(SystemWantCSMsg msg) {
         logReceivingMsg("SystemWantCSMsg", -1);
-        request_queue.add(getSelf());
+        request_queue.add(new ActorInfo(getSelf(), id));
         assignPrivilege();
         makeRequest();
     }
@@ -331,20 +366,20 @@ class Node extends AbstractActor {
      */
 
     private void makeRequest() {
-        if ((holder != getSelf()) && (!request_queue.isEmpty()) && (!asked)) {
-            logSendingMsg("Request", -1);
-            holder.tell(new RequestMsg(), getSelf());
+        if ((holder.reference != getSelf()) && (!request_queue.isEmpty()) && (!asked)) {
+            logSendingMsg("Request", holder.id);
+            holder.reference.tell(new RequestMsg(id), getSelf());
             asked = true;
         }
     }
 
     private void assignPrivilege() {
-        if ((holder == getSelf()) && (!using) && (!request_queue.isEmpty())) {
+        if ((holder.reference == getSelf()) && (!using) && (!request_queue.isEmpty())) {
             holder = request_queue.removeFirst();
             asked = false;
 
             // if we are the holder we can use the CS, if interested
-            if (holder == getSelf()) {
+            if (holder.reference == getSelf()) {
                 using = true;
 
                 // simulate CS duration by a message that is sent to us in the future
@@ -356,8 +391,8 @@ class Node extends AbstractActor {
                 System.out.print(to_log);
             } else {
                 // if we are not the new holder send the Privilege to the new one
-                logSendingMsg("Privilege", -1);
-                holder.tell(new PrivilegeMsg(), getSelf());
+                logSendingMsg("Privilege", holder.id);
+                holder.reference.tell(new PrivilegeMsg(id), getSelf());
             }
         }
     }
@@ -373,6 +408,17 @@ class Node extends AbstractActor {
         String to_log = String.format("Node %02d sent %s msg to node %02d\n", this.id, msg_type, to_id);
         history.append(to_log);
         System.out.print(to_log);
+    }
+
+    // utility classes
+    class ActorInfo {
+        public final ActorRef reference;
+        public final int id;
+
+        public ActorInfo(final ActorRef reference, final int id) {
+            this.reference = reference;
+            this.id = id;
+        }
     }
 
 }
