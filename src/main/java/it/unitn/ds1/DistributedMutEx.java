d88f88d79e10e597ca7e0c8f86e280eb07d54a69
katerynak <konotopska.k@gmail.com>
2019-05-11 15:54:10 +0200

    added user interactive loop

--- src/main/java/it/unitn/ds1/DistributedMutEx.java
+++ src/main/java/it/unitn/ds1/DistributedMutEx.java
@@ -11,6 +11,8 @@ import java.util.Collections;
 import java.io.IOException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 import it.unitn.ds1.Node;
 import it.unitn.ds1.Node.SystemInitMsg;
@@ -24,7 +26,7 @@ assunto che no nodo crasha durante init?
 assertions
 metodo per fare un albero in maniera pulita (dottorando del tizio diceva a compile time, da qualche file di confi? di
 akka?)
-mettere if fail/recovery then etc, attualmente sti check nn ci sono
+mettere if fail/recovery then etc, attualmente sti check nn ci sono --> fatto
 aggiunere id di sender a messaggi node to node
 in init, failure e recovery settare bene tutti i campi, nn fatto per init e failure
  */
@@ -74,29 +76,60 @@ public class DistributedMutEx {
         SystemInitMsg init = new SystemInitMsg();
         nodes.get(0).tell(init, null);
 
-        try {
-            System.out.println(">>> Wait for init to stop then ENTER to make 9 ask for CS <<<");
-            System.in.read();
-        } catch (IOException ioe) {
+        boolean exit = false;
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        String user_in = new String();
+        String action = new String();
+        Integer action_node = new Integer(1);
+        boolean valid_in = true;
+
+        while (!exit) {
+            valid_in = true;
+            try {
+                System.out.println(">>> Type <action><nodeId> in order to say what you want to do <<<");
+                System.out.println(">>> Actions: 'c', ask for CS ; 'f', fail <<<");
+                System.out.printf(">>> Node ids: 0 - %d <<<\n", N_ACTORS-1);
+                System.out.println(">>> Or type 'e' to exit <<<\n");
+
+                user_in = reader.readLine();
+
+            } catch (IOException ioe) {
+            }
+            if (user_in.length()>0) {
+                action = user_in.substring(0, 1);
+
+                if (!action.equals("e"))
+                    action_node = Integer.parseInt(user_in.substring(1, user_in.length()));
+            }
+            else
+                valid_in = false;
+
+            if ((!(action.equals("c")) && !(action.equals("f")) && !(action.equals("e"))) || (action_node < 0 || action_node >= N_ACTORS ))
+                valid_in = false;
+
+            if (!valid_in)
+                System.out.println(">>> Not valid action code <<<");
+            else {
+                switch (action){
+                    case "e":
+                        exit = true;
+                        break;
+                    case "c":
+                        SystemWantCSMsg wantCS = new SystemWantCSMsg();
+                        nodes.get(action_node).tell(wantCS, null);
+                        break;
+                    case "f":
+                        SystemFailMsg fail = new SystemFailMsg();
+                        nodes.get(action_node).tell(fail, null);
+                        break;
+                }
+
+            }
+
         }
-
-        SystemWantCSMsg wantCS = new SystemWantCSMsg();
-        nodes.get(9).tell(wantCS, null);
-
         try {
-            System.out.println(">>> ENTER to make 3 fail <<<");
-            System.in.read();
-        } catch (IOException ioe) {
-        }
-
-        SystemFailMsg fail = new SystemFailMsg();
-        nodes.get(3).tell(fail, null);
-
-        // qua poi va fatto loop iterativo su input dell utente, per fare entrare nodi in cs, stoppare, etc.
-        //
-        try {
-            System.out.println(">>> Wait for the chats to stop and press ENTER <<<");
-            System.in.read();
 
             SystemPrintHistoryMsg msg = new SystemPrintHistoryMsg();
             for (ActorRef peer : nodes) {
@@ -106,6 +139,7 @@ public class DistributedMutEx {
             System.in.read();
         } catch (IOException ioe) {
         }
+
         system.terminate();
     }
 }
